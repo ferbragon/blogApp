fe40e15722b91fb977bdccc2b043814c2089d5ef
ferbragon <nando.gon17@gmail.com>
2024-01-23 13:00:31 -0600

    feat(src/components/NavBar.tsx): Add global state to set buttons

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,13 @@
 "use client";
 import React, { useEffect, useState, useRef } from "react";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks/hooks";
 import { motion } from "framer-motion";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import searchIcon from "../../public/search-icon.png";
+import Image from "next/image";
+import { hideSearch } from "@/redux/reducer/actions";
+import InternetStatus from "./ui/InternetStatus";
 
 // Variants for the navbar animation
 const variants = {
@@ -11,11 +17,21 @@ const variants = {
 
 // Navbar component
 const NavBar = ({}) => {
+  const dispatch = useAppDispatch();
+  const pathname = usePathname();
+  // Global state
+  /* The navbarHidden state will allow us to display the Search component if the screen*/
+  const searchHidden = useAppSelector((state) => state.myReducer.searchHidden);
+  /* The createPostHidden state will allow us to display the CreatePost component if the screen*/
+  const createPostHidden = useAppSelector(
+    (state) => state.myReducer.createPostHidden
+  );
+  const internetConnected = useAppSelector(
+    (state) => state.myReducer.internetConnected
+  );
   // Local state
   // The state showNavbar will allow us to hide or show the navbar during the scroll animation
   const [showNavbar, setShowNavbar] = useState(true);
-  /* The showSearch state will allow us to display the Search component if the screen*/
-  const [showSearch, setShowSearch] = useState(false);
   /* The topStyles state will allow us to control the fade-out animation of the navbar along with the scroll animation
   also the navbar buttons text color and background buttons  */
   const [topStyles, setTopStyles] = useState(true);
@@ -54,36 +70,51 @@ const NavBar = ({}) => {
     };
   }, []);
 
-  // Function to showMenu
+  // Show search and hide navbar
   const handleShowSearch = () => {
-    setShowSearch(!showSearch);
-
-    document.body.style.overflowY = showSearch ? "auto" : "hidden";
+    dispatch(hideSearch(false));
   };
 
   return (
     <>
-      {/* // If the screen is larger than 1100 px, the Navbar is displayed with all its buttons. */}
-      <motion.div
-        className={`transition-all duration-1000 sticky top-0 w-full h-20 flex flex-row items-center justify-center z-10 ${
-          topStyles ? "b-transparent" : "background"
-        }`}
-        variants={variants}
-        initial="hidden"
-        animate={showNavbar ? "visible" : "hidden"}
-        transition={{ duration: 0 }}
-      >
-        <div className="w-full flex justify-between padding">
-          <div className="w-6/10 h-full flex flex-row items-center justify-end">
-            <span className={`logo ${topStyles ? "text-[#000]" : ""}`}>
-              Wifi
-            </span>
+      {searchHidden && createPostHidden ? (
+        <motion.div
+          className={`transition-all duration-1000 sticky top-0 w-full h-20 flex flex-row items-center justify-center z-10 ${
+            topStyles ? "b-transparent" : "background"
+          }`}
+          variants={variants}
+          initial="hidden"
+          animate={showNavbar ? "visible" : "hidden"}
+          transition={{ duration: 0 }}
+        >
+          <div className="w-full flex justify-between padding">
+            <div className="w-6/10 h-full flex flex-row items-center justify-end">
+              {pathname === "/" ? (
+                <span className={`${topStyles ? "text-[#000]" : ""}`}>
+                  <InternetStatus />
+                </span>
+              ) : (
+                <Link
+                  className="cursor-pointer font-bold hover:text-blue-600"
+                  href="/"
+                >
+                  &larr; Go back
+                </Link>
+              )}
+            </div>
+            <div className="w-3/10 h-full flex items-center justify-end pr-30">
+              {pathname === "/" && internetConnected ? (
+                <Image
+                  onClick={handleShowSearch}
+                  src={searchIcon}
+                  alt="search icon"
+                  className="w-[30px] h-[30px] objetct-cover cursor-pointer"
+                />
+              ) : null}
+            </div>
           </div>
-          <div className="w-3/10 h-full flex items-center justify-end pr-30">
-            Search button
-          </div>
-        </div>
-      </motion.div>
+        </motion.div>
+      ) : null}
     </>
   );
 };
