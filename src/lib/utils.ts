73e1e6c892ffb60b8c14d6cfc3457347708cd3dc
ferbragon <nando.gon17@gmail.com>
2024-01-23 12:59:25 -0600

    feat(src/lib/utils.ts): Add sliceContent function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,10 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function sliceContent(content: string) {
+  return content.length >= 70 ? `${content.slice(0, 70)}...` : content;
 }
