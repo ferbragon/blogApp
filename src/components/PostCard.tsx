1b1bdd0002a0b66d0471858f6775d4725a8036e0
ferbragon <nando.gon17@gmail.com>
2024-01-23 12:59:56 -0600

    feat(src/components/PostCard.tsx): Add PostCard component

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { IPostCardProps } from "@/types/types";
+import Link from "next/link";
+import { sliceContent } from "@/lib/utils";
 
 const PostCard: React.FC<IPostCardProps> = ({
   id,
@@ -9,13 +11,16 @@ const PostCard: React.FC<IPostCardProps> = ({
   createdAt,
 }) => {
   return (
-    <div className="bg-white rounded-lg flex flex-col items-center justify-between w-[300px] h-[300px] border p-3 cursor-pointer">
+    <Link
+      className="bg-white rounded-lg flex flex-col items-center justify-between w-[300px] h-[300px] border p-3 cursor-pointer"
+      href={`/post/${id}`}
+    >
       <div className="w-full flex flex-col items-center justify-center">
-        <h4 className="w-full text-left text-2x1 font-bold mb-2">{title}</h4>
-        <h3 className="w-full text-left text-lg font-bold">{author}</h3>
+        <h4 className="w-full text-left text-x1 font-bold mb-2">{title}</h4>
+        <h5 className="w-full text-left text-lg font-bold">{author}</h5>
       </div>
-      <p>{content}</p>
-    </div>
+      <p>{sliceContent(content)}</p>
+    </Link>
   );
 };
 
