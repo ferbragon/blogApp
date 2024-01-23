d7103fff39913e63fe7c98a421362021184a7b87
ferbragon <nando.gon17@gmail.com>
2024-01-23 12:57:17 -0600

    feat(src/redux/reducer/reducer.ts): Add SEARCH_HIDDEN, CREATE_POST_HIDDEN and INTERNET_CONNECTED cases

--- src/redux/reducer/reducer.ts
+++ src/redux/reducer/reducer.ts
@@ -1,7 +1,16 @@
 import { IInitialState, IAction } from "../../types/types";
+import {
+  CREATE_POST_HIDDEN,
+  INTERNET_CONNECTED,
+  SEARCH_HIDDEN,
+  UPDATE_POST,
+} from "./reduxtypes";
 
 const initialState: IInitialState = {
   data: null,
+  searchHidden: true,
+  createPostHidden: true,
+  internetConnected: true,
 };
 
 const reducer = (
@@ -9,11 +18,34 @@ const reducer = (
   action: IAction | any
 ): IInitialState => {
   switch (action.type) {
-    // case SELECT_LANGUAGE:
-    //   return {
-    //     ...state,
-    //     language: action.payload === "es" ? "es" : "en",
-    //   };
+    case UPDATE_POST:
+      if (typeof action.payload === "object") {
+        return {
+          ...state,
+          data: action.payload,
+        };
+      }
+    case SEARCH_HIDDEN:
+      if (typeof action.payload === "boolean") {
+        return {
+          ...state,
+          searchHidden: action.payload,
+        };
+      }
+    case CREATE_POST_HIDDEN:
+      if (typeof action.payload === "boolean") {
+        return {
+          ...state,
+          createPostHidden: action.payload,
+        };
+      }
+    case INTERNET_CONNECTED:
+      if (typeof action.payload === "boolean") {
+        return {
+          ...state,
+          internetConnected: action.payload,
+        };
+      }
     default:
       return state;
   }
