57675bcdd135e4b570fdb52a76fc124eac2cde72
ferbragon <nando.gon17@gmail.com>
2024-01-23 12:58:01 -0600

    feat(src/redux/reducer/actions.ts): Add hideCreatePost and changeInternetStatus actions

--- src/redux/reducer/actions.ts
+++ src/redux/reducer/actions.ts
@@ -1,1 +1,35 @@
+import {
+  SEARCH_HIDDEN,
+  UPDATE_POST,
+  CREATE_POST_HIDDEN,
+  INTERNET_CONNECTED,
+} from "./reduxtypes";
+import { IPostCardProps } from "@/types/types";
 
+export const hideSearch = (hide: boolean) => {
+  return {
+    type: SEARCH_HIDDEN,
+    payload: hide,
+  };
+};
+
+export const updatePost = (posts: IPostCardProps[]) => {
+  return {
+    type: UPDATE_POST,
+    payload: posts,
+  };
+};
+
+export const hideCreatePost = (hide: boolean) => {
+  return {
+    type: CREATE_POST_HIDDEN,
+    payload: hide,
+  };
+};
+
+export const changeInternetStatus = (status: boolean) => {
+  return {
+    type: INTERNET_CONNECTED,
+    payload: status,
+  };
+};
